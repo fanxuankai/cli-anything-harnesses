8af39b881353561530d3a520ed8998c9398f69b3
Jolc <fan_xuankai@126.com>
2026-04-22 13:47:44 +0800

    fix

--- agent-harness/mediasaber/setup.py
+++ agent-harness/mediasaber/setup.py
@@ -3,7 +3,7 @@ from setuptools import find_namespace_packages, setup
 
 setup(
     name="cli-anything-mediasaber",
-    version="1.0.0",
+    version="1.0.1",
     description="CLI harness for Media Saber",
     packages=find_namespace_packages(include=["cli_anything.*"]),
     include_package_data=True,
